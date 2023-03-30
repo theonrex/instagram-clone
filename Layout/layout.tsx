3db31f961006ba3af48daf9f4f39a63fe9970255
theonrex <theonrex1@gmail.com>
2023-03-30 12:22:16 +0100

    maade images responsive

--- Layout/layout.tsx
+++ Layout/layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-
+import Head from "next/head";
 import Navbar from "@/components/NavBar/Navbar";
 import BottomNav from "../components/BottomNav/bottomNav";
 
@@ -10,9 +10,18 @@ type LayoutProps = {
 export default function Layout({ children }: LayoutProps) {
   return (
     <>
+      <Head>
+        <meta
+          name="description"
+          content="Welcome to the best social media platform."
+        />
+       
+        <link rel="shortcut icon" href="/favicon.png" />
+        <title>  Instagram</title>
+      </Head>
       <Navbar />
       {children}
-      <BottomNav/>
+      <BottomNav />
     </>
   );
 }
